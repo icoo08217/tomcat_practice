d44709f926ac32812796aae67f954e07b1996d68
icoo08217 <icoo08216@gmail.com>
2022-07-25 15:01:26 +0900

    게시물 작성폼 작성

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -31,6 +31,9 @@ public class DispatchServlet extends HttpServlet {
             case "/usr/member/login":
                 memberController.showLogin(rq);
                 break;
+            case "/usr/article/write/free":
+                articleController.showWrite(rq);
+                break;
         }
     }
 }
